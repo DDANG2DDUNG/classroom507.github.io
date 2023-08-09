e0c5f79b01ae62441477ff73cc2c50c1540ff484
DDANG2DDUNG <141654786+DDANG2DDUNG@users.noreply.github.com>
2023-08-10 02:15:09 +0900

    update all

--- JAVASCRIPT/set_pop.js
+++ JAVASCRIPT/set_pop.js
@@ -1,4 +1,6 @@
 
+
+
 function set_pop_none(chkValue){
     $('#POP'+chkValue).css('display', 'none');
  
@@ -9,18 +11,34 @@ function set_pop_block(chkValue){
     
 }
 
+function map_hide(){
+    $('#POP3').css('visibility', 'hidden');
+}
+
+function map_show(){
+    $('#POP3').css('visibility', 'show');
+}
 
 var list=['1', '2', '3', '4'];
 
 function set_radio_pop(chkValue){
     for(i=0; i<list.length; i++){
         if(list[i]==chkValue) {
+           if(chkValue=='3')
+            map_show();
+           else
             set_pop_block(chkValue);
+
+
             set_rad_chk(chkValue);
         }
         else{
-            set_pop_none(list[i]);
+            if(list[i]=='3') map_hide();
+
+            else set_pop_none(list[i]);
             set_rad_init(list[i]);
         }
+   
     }
 }
+
