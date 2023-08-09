7a2d5f4b67a90ac68c8159b80b59fd655d74dc0e
DDANG2DDUNG <141654786+DDANG2DDUNG@users.noreply.github.com>
2023-08-10 00:48:25 +0900

    update all

--- JAVASCRIPT/set_radio.js
+++ JAVASCRIPT/set_radio.js
@@ -1,9 +1,11 @@
 function set_rad_init(chkValue){
     $('#RAD'+chkValue).css('border-color', 'white');
     $('#RAD'+chkValue).css('background-color', 'white');
+    $('#RAD'+chkValue).css('color', 'rgba(37,58,46, 1)ß');
 }
 
 function set_rad_chk(chkValue){
-    $('#RAD'+chkValue).css('border-color', 'red');
-    $('#RAD'+chkValue).css('background-color', 'red');
+    $('#RAD'+chkValue).css('border-color', 'rgba(37,58,46, 1)');
+    $('#RAD'+chkValue).css('background-color', 'rgba(37,58,46, 1)');
+    $('#RAD'+chkValue).css('color', 'rgba(248,245,235,1)');
 }
